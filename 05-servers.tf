d863a7c92611615c55e420434fc68e2dc42bdd7b
Andrew Siwko <asiwko@siwko.org>
2026-02-09 16:20:49 -0500

    Use Oracle linux instead of RHEL ; change vm name

--- 05-servers.tf
+++ 05-servers.tf
@@ -2,12 +2,11 @@ data "oci_identity_availability_domains" "ads" {
   compartment_id = var.compartment_id
 }
 
-resource "oci_core_instance" "rhel_vm" {
+resource "oci_core_instance" "asiwko_vm" {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   compartment_id      = var.compartment_id
-  display_name        = "RHEL9-Production-VM"
-  
-  # Shape selection (Flex shapes are standard for RHEL 9)
+  display_name        = "asiwko-vm-01"
+
   shape = "VM.Standard.E4.Flex"
   shape_config {
     ocpus         = 1
@@ -23,15 +22,15 @@ resource "oci_core_instance" "rhel_vm" {
 
   source_details {
     source_type = "image"
-    # REPLACE THIS with the RHEL 9.x OCID from your region
-    source_id   = "ocid1.image.oc1.iad.aaaaaaa..." 
+    # Fix: Use the Oracle Linux 9.7 OCID from your verified list
+    source_id               = "ocid1.image.oc1.iad.aaaaaaaahvwnjutyewvsr2nkjcfq6i7l5anlm6bslnoom2vaerhwbzc2wx4a"
     boot_volume_size_in_gbs = 50
   }
 
   metadata = {
     # Provide your SSH public key to access the instance
     ssh_authorized_keys = file(var.ssh_public_key_path)
-    
+
     # Optional: user_data for cloud-init (e.g., register with Red Hat Subscription Manager)
     # user_data = base64encode(file("cloud-init.sh"))
   }
