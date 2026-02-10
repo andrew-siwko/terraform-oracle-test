a48f0786e33bd287d22724941a7c3aba8b88e84c
Andrew Siwko <asiwko@siwko.org>
2026-02-09 22:24:28 -0500

    add dns labels to resolve network issue

--- 03-network.tf
+++ 03-network.tf
@@ -2,17 +2,19 @@ resource "oci_core_vcn" "asiwko_vcn" {
   compartment_id = var.tenancy_ocid
   cidr_block     = "10.0.0.0/16"
   display_name   = "asiwko-vcn"
+  dns_label      = "asiwkovcn"
 }
 
 resource "oci_core_subnet" "asiwko_subnet" {
-  compartment_id = var.tenancy_ocid
+  compartment_id      = var.tenancy_ocid
   vcn_id              = oci_core_vcn.asiwko_vcn.id
   display_name        = "asiwko-subnet"
   cidr_block          = "10.0.1.0/24"
   security_list_ids   = [oci_core_security_list.web_security_list.id]
   route_table_id      = oci_core_vcn.asiwko_vcn.default_route_table_id
+  depends_on          = [oci_core_default_route_table.asiwko_rt]
+  dns_label           = "asiwkosubnet"
   prohibit_public_ip_on_vnic = false
-  depends_on = [oci_core_default_route_table.asiwko_rt]
 }
 
 resource "oci_core_internet_gateway" "asiwko_ig" {
@@ -28,7 +30,5 @@ resource "oci_core_default_route_table" "asiwko_rt" {
     destination       = "0.0.0.0/0"
     destination_type  = "CIDR_BLOCK"
     network_entity_id = oci_core_internet_gateway.asiwko_ig.id
-    # this was present in the old (2020) route
-    cidr_block        = "0.0.0.0/0"
   }
 }
