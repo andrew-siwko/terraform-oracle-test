24fefdf790187f4e4678de35c606d5bc2d5d8968
Andrew Siwko <asiwko@siwko.org>
2026-02-09 22:34:08 -0500

    restore dns and output

--- 07-outputs.tf
+++ 07-outputs.tf
@@ -1,7 +1,3 @@
-# output "instance_public_ip" {
-#   value = oci_core_instance.asiwko_vm.public_ip
-# }
-
-output "TEST" {
-  value       = "Can't be blank!"
+output "instance_public_ip" {
+  value = oci_core_instance.asiwko_vm.public_ip
 }
