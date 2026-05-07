bbbf49677c4ca28bd63bd6bfa2e4b621352db131
Andrew Siwko <asiwko@siwko.org>
2026-05-07 15:36:57 -0400

    too many dependencies

--- 06-servers.tf
+++ 06-servers.tf
@@ -2,35 +2,35 @@ data "oci_identity_availability_domains" "ads" {
   compartment_id = var.tenancy_ocid
 }
 
-# resource "oci_core_instance" "siwko_vm" {
-#   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
-#   compartment_id      = var.tenancy_ocid
-#   display_name        = "asiwko-vm-01"
-#   # shape               = var.instance_shape
-#   shape               = local.free_shape_name
+resource "oci_core_instance" "siwko_vm" {
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
+  compartment_id      = var.tenancy_ocid
+  display_name        = "asiwko-vm-01"
+  # shape               = var.instance_shape
+  shape               = local.free_shape_name
 
-#   create_vnic_details {
-#     assign_public_ip = true
-#     subnet_id        = oci_core_subnet.asiwko_subnet.id
-#     hostname_label   = "asiwko-vm-01"
-#   }
+  create_vnic_details {
+    assign_public_ip = true
+    subnet_id        = oci_core_subnet.asiwko_subnet.id
+    hostname_label   = "asiwko-vm-01"
+  }
 
-#   source_details {
-#     source_type             = "image"
-#     # source_id               = var.image_ocid
-#     source_id               = local.latest_arm_image_id
-#     boot_volume_size_in_gbs = 50
-#   }
+  source_details {
+    source_type             = "image"
+    # source_id               = var.image_ocid
+    source_id               = local.latest_arm_image_id
+    boot_volume_size_in_gbs = 50
+  }
 
-#   # added this for the flexible shape type.
-#   shape_config {
-#     ocpus         = 1
-#     memory_in_gbs = 6 
-#   }
+  # added this for the flexible shape type.
+  shape_config {
+    ocpus         = 1
+    memory_in_gbs = 6 
+  }
 
-#   metadata = {
-#     ssh_authorized_keys = file(var.ssh_public_key_path)
-#   }
+  metadata = {
+    ssh_authorized_keys = file(var.ssh_public_key_path)
+  }
 
-# }
+}
 
