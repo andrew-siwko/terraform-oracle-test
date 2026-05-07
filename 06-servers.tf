f0eeafa3c8c0c92e9642321ba6edfb212d9d58db
Andrew Siwko <asiwko@siwko.org>
2026-05-07 14:20:06 -0400

    go back tot he old parameters to get tfstate reset

--- 06-servers.tf
+++ 06-servers.tf
@@ -6,20 +6,19 @@ resource "oci_core_instance" "asiwko_vm" {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
   compartment_id      = var.tenancy_ocid
   display_name        = "asiwko-vm-01"
-  # shape               = var.instance_shape
-  shape               = local.free_shape_name
+  shape               = var.instance_shape
+  # shape               = local.free_shape_name
 
   create_vnic_details {
     assign_public_ip = true
     subnet_id        = oci_core_subnet.asiwko_subnet.id
-    # subnet_id        = var.subnet_ocid
     hostname_label   = "asiwko-vm-01"
   }
 
   source_details {
     source_type             = "image"
-    # source_id               = var.image_ocid
-    source_id               = local.latest_arm_image_id
+    source_id               = var.image_ocid
+    # source_id               = local.latest_arm_image_id
     boot_volume_size_in_gbs = 50
   }
 
