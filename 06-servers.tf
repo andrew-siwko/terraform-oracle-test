76db4f0dc22ed805add426760cf1ce1ffb7ff398
Andrew Siwko <asiwko@siwko.org>
2026-05-07 13:18:37 -0400

    moved from hardcoded shape/image to data driven

--- 06-servers.tf
+++ 06-servers.tf
@@ -6,7 +6,8 @@ resource "oci_core_instance" "asiwko_vm" {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
   compartment_id      = var.tenancy_ocid
   display_name        = "asiwko-vm-01"
-  shape               = var.instance_shape
+  # shape               = var.instance_shape
+  shape               = local.free_shape_name
 
   create_vnic_details {
     assign_public_ip = true
@@ -17,7 +18,8 @@ resource "oci_core_instance" "asiwko_vm" {
 
   source_details {
     source_type             = "image"
-    source_id               = var.image_ocid
+    # source_id               = var.image_ocid
+    source_id               = local.latest_arm_image_id
     boot_volume_size_in_gbs = 50
   }
 
